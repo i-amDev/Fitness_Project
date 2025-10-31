781586f9cbffa23a765509912ecf324161e92773
i-amDev <92856531+i-amDev@users.noreply.github.com>
2025-10-31 17:02:59 +0530

    - Created the "User" entity. - Also created the "UserRole" enum to use the role while saving the user.

---
+++ user-service/src/main/java/com/fitnessProject/user_service/entity/User.java
@@ -0,0 +1,38 @@
+package com.fitnessProject.user_service.entity;
+
+import com.fitnessProject.user_service.roles.UserRole;
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String firstName;
+
+    private String lastName;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role = UserRole.USER;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
---
+++ user-service/src/main/java/com/fitnessProject/user_service/roles/UserRole.java
@@ -0,0 +1,5 @@
+package com.fitnessProject.user_service.roles;
+
+public enum UserRole {
+    ADMIN, USER
+}
