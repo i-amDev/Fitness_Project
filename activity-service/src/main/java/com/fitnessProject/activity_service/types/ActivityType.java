ff13e54b21d400c426a71a593ff1d37038ce66ea
i-amDev <92856531+i-amDev@users.noreply.github.com>
2025-11-01 14:06:09 +0530

    - Created the "Activity" entity class. - Also created the "ActivityType" enum.

---
+++ activity-service/src/main/java/com/fitnessProject/activity_service/types/ActivityType.java
@@ -0,0 +1,14 @@
+package com.fitnessProject.activity_service.types;
+
+public enum ActivityType {
+    RUNNING,
+    WALKING,
+    CYCLING,
+    SWIMMING,
+    WEIGHT_TRAINING,
+    YOGA,
+    HIIT,
+    CARDIO,
+    STRETCHING,
+    OTHER
+}
