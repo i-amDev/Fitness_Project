08dfdb7887daf5b073c4068b92bba5a5209a2fd7
i-amDev <92856531+i-amDev@users.noreply.github.com>
2025-11-02 17:38:37 +0530

    - After processing the data, used the "RecommendationRepository" to save the recommendation generated from the AI into the DB.

--- ai-service/src/main/java/com/fitnessProject/ai_service/service/ActivityMessageListener.java
+++ ai-service/src/main/java/com/fitnessProject/ai_service/service/ActivityMessageListener.java
@@ -1,6 +1,8 @@
 package com.fitnessProject.ai_service.service;
 
 import com.fitnessProject.ai_service.model.Activity;
+import com.fitnessProject.ai_service.model.Recommendation;
+import com.fitnessProject.ai_service.repository.RecommendationRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -13,10 +15,13 @@ public class ActivityMessageListener {
 
     private final ActivityAIService activityAIService;
 
+    private final RecommendationRepository recommendationRepository;
+
     @KafkaListener(topics = "${kafka.topic.name}", groupId = "activity-processor-group")
     public void processActivity(Activity activity) {
         log.info("Received activity for processing {}", activity.getUserId());
-        activityAIService.generateRecommendation(activity);
+        Recommendation recommendation = activityAIService.generateRecommendation(activity);
+        recommendationRepository.save(recommendation);
     }
 
 
