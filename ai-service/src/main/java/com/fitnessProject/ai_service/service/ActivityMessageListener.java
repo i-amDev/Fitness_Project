696bb37df7a87c95f00e2e2fe8553fe0bf9f16b5
i-amDev <92856531+i-amDev@users.noreply.github.com>
2025-11-02 16:10:49 +0530

    - Used the "ActivityAIService" to generate the recommendations from the activity.

--- ai-service/src/main/java/com/fitnessProject/ai_service/service/ActivityMessageListener.java
+++ ai-service/src/main/java/com/fitnessProject/ai_service/service/ActivityMessageListener.java
@@ -11,9 +11,12 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class ActivityMessageListener {
 
+    private final ActivityAIService activityAIService;
+
     @KafkaListener(topics = "${kafka.topic.name}", groupId = "activity-processor-group")
     public void processActivity(Activity activity) {
         log.info("Received activity for processing {}", activity.getUserId());
+        activityAIService.generateRecommendation(activity);
     }
 
 
