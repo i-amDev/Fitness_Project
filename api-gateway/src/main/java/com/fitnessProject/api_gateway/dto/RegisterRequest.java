ed567b0ec686bb6500a195f555061db54bc9186a
i-amDev <92856531+i-amDev@users.noreply.github.com>
2025-11-04 16:36:27 +0530

    - Created "RegisterRequest" and "UserResponse" dto in "api-gateway".

---
+++ api-gateway/src/main/java/com/fitnessProject/api_gateway/dto/RegisterRequest.java
@@ -0,0 +1,24 @@
+package com.fitnessProject.api_gateway.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class RegisterRequest {
+
+    @NotBlank(message = "Email is required!")
+    @Email(message = "Invalid email format")
+    private String email;
+
+    private String keyCloakId;
+
+    @NotBlank(message = "Password is required!")
+    @Size(min = 3, message = "Password must have at-least 3 characters.")
+    private String password;
+
+    private String firstName;
+
+    private String lastName;
+}
---
+++ api-gateway/src/main/java/com/fitnessProject/api_gateway/dto/UserResponse.java
@@ -0,0 +1,25 @@
+package com.fitnessProject.api_gateway.dto;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class UserResponse {
+
+    private String id;
+
+    private String keyCloakId;
+
+    private String email;
+
+    private String password;
+
+    private String firstName;
+
+    private String lastName;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+}
