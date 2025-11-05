4bf7c31a8a22af28cb0018efee8065d7ce248be6
i-amDev <92856531+i-amDev@users.noreply.github.com>
2025-11-05 15:26:10 +0530

    - Getting an error, hence instead of using modelMapper to map the objects, used manual creation of objects and setting the values in them manually.

--- user-service/src/main/java/com/fitnessProject/user_service/service/UserService.java
+++ user-service/src/main/java/com/fitnessProject/user_service/service/UserService.java
@@ -23,14 +23,44 @@ public class UserService {
         if (userRepository.existsByEmail(registerRequest.getEmail())) {
 //            throw new RuntimeException("User already exist with the given email!!");
             User existingUser = userRepository.findByEmail(registerRequest.getEmail());
-            return modelMapper.map(existingUser, UserResponse.class);
+//            return modelMapper.map(existingUser, UserResponse.class);
+            UserResponse userResponse = new UserResponse();
+            userResponse.setId(existingUser.getId());
+            userResponse.setKeyCloakId(existingUser.getKeyCloakId());
+            userResponse.setPassword(existingUser.getPassword());
+            userResponse.setEmail(existingUser.getEmail());
+            userResponse.setFirstName(existingUser.getFirstName());
+            userResponse.setLastName(existingUser.getLastName());
+            userResponse.setCreatedAt(existingUser.getCreatedAt());
+            userResponse.setUpdatedAt(existingUser.getUpdatedAt());
+            return userResponse;
         }
 
-        User entity = modelMapper.map(registerRequest, User.class);
+        User user = new User();
+        user.setEmail(registerRequest.getEmail());
+        user.setPassword(registerRequest.getPassword());
+        user.setKeyCloakId(registerRequest.getKeyCloakId());
+        user.setFirstName(registerRequest.getFirstName());
+        user.setLastName(registerRequest.getLastName());
 
-        User savedEntity = userRepository.save(entity);
+        User savedUser = userRepository.save(user);
+        UserResponse userResponse = new UserResponse();
+        userResponse.setKeyCloakId(savedUser.getKeyCloakId());
+        userResponse.setId(savedUser.getId());
+        userResponse.setPassword(savedUser.getPassword());
+        userResponse.setEmail(savedUser.getEmail());
+        userResponse.setFirstName(savedUser.getFirstName());
+        userResponse.setLastName(savedUser.getLastName());
+        userResponse.setCreatedAt(savedUser.getCreatedAt());
+        userResponse.setUpdatedAt(savedUser.getUpdatedAt());
 
-        return modelMapper.map(savedEntity, UserResponse.class);
+        return userResponse;
+
+//        User entity = modelMapper.map(registerRequest, User.class);
+//
+//        User savedEntity = userRepository.save(entity);
+//
+//        return modelMapper.map(savedEntity, UserResponse.class);
     }
 
     public UserResponse getUserProfile(String userId) {
