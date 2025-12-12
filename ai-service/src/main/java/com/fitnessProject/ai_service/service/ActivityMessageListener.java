18d856ad14ee3c54b72288fac4fc1919044701a3
i-amDev <92856531+i-amDev@users.noreply.github.com>
2025-12-12 12:16:12 +0530

    - Refactored. - Removed some extra lines.

--- ai-service/src/main/java/com/fitnessProject/ai_service/service/ActivityMessageListener.java
+++ ai-service/src/main/java/com/fitnessProject/ai_service/service/ActivityMessageListener.java
@@ -23,6 +23,4 @@ public class ActivityMessageListener {
         Recommendation recommendation = activityAIService.generateRecommendation(activity);
         recommendationRepository.save(recommendation);
     }
-
-
 }
