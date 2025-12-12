5353f723fd54a10cef83b07367edde6fda5ab2b2
i-amDev <92856531+i-amDev@users.noreply.github.com>
2025-12-12 12:13:15 +0530

    - Refactored. - Removed unused imports.

--- ai-service/src/main/java/com/fitnessProject/ai_service/model/Activity.java
+++ ai-service/src/main/java/com/fitnessProject/ai_service/model/Activity.java
@@ -7,7 +7,6 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
 import java.time.LocalDateTime;
